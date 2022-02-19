2e088803e716fe437f21ef9ba956e8bd42950e14
vlad <vlad@DESKTOP-MPMNN79.localdomain>
2022-02-19 23:45:07 +0200

    almost working log search

--- gui/elements/colorize.go
+++ gui/elements/colorize.go
@@ -1,19 +1,44 @@
 package elements
 
-type Color string
+type FColor string
+type BColor string
 
 const (
-	reset        = "\033[0m"
-	Red    Color = "\033[31m"
-	Green  Color = "\033[32m"
-	Yellow Color = "\033[33m"
-	Blue   Color = "\033[34m"
-	Purple Color = "\033[35m"
-	Cyan   Color = "\033[36m"
-	Gray   Color = "\033[37m"
-	White  Color = "\033[97m"
+	reset                = "\033[0m"
+	Red           FColor = "\033[31m"
+	Green         FColor = "\033[32m"
+	Yellow        FColor = "\033[33m"
+	Blue          FColor = "\033[34m"
+	Purple        FColor = "\033[35m"
+	Cyan          FColor = "\033[36m"
+	Gray          FColor = "\033[37m"
+	White         FColor = "\033[97m"
+	B_Black       BColor = "\033[40m"
+	B_Red         BColor = "\033[41m"
+	B_Green       BColor = "\033[42m"
+	B_Orange      BColor = "\033[43m"
+	B_Blue        BColor = "\033[44m"
+	B_Magenta     BColor = "\033[45m"
+	B_Cyan        BColor = "\033[46m"
+	B_LightGray   BColor = "\033[47m"
+	B_DarkGray    BColor = "\033[100m"
+	B_LightRed    BColor = "\033[101m"
+	B_LightGreen  BColor = "\033[102m"
+	B_Yellow      BColor = "\033[103m"
+	B_LightBlue   BColor = "\033[104m"
+	B_LightPurple BColor = "\033[105m"
+	B_Teal        BColor = "\033[106m"
+	B_White       BColor = "\033[107m"
 )
 
-func Colorize(str string, color Color) string {
+func Foreground(str string, color FColor) string {
 	return string(color) + str + string(reset)
+}
+
+func Background(str string, color BColor) string {
+	return string(color) + str + string(reset)
+}
+
+func Colorize(str string, f_color FColor, b_color BColor) string {
+	return string(b_color) + string(f_color) + str + string(reset)
 }
--- gui/window/container_log_window.go
+++ gui/window/container_log_window.go
@@ -1,6 +1,7 @@
 package window
 
 import (
+	"bufio"
 	"context"
 	"dc-top/docker"
 	"dc-top/gui/elements"
@@ -8,19 +9,41 @@ import (
 	"dc-top/utils"
 	"fmt"
 	"log"
+	"os"
+	"regexp"
+	"strings"
+	"sync"
 
 	"github.com/eiannone/keyboard"
 	"github.com/gdamore/tcell/v2"
 )
 
-type ContainerLogWindow struct {
-	id      string
-	context context.Context
-	cancel  context.CancelFunc
-}
+type searchMode uint8
+
+const (
+	str searchMode = iota
+	regex
+)
+
 type logsWriter struct {
-	draw_queue chan []byte
-	screen     tcell.Screen
+	screen             tcell.Screen
+	search_mode        searchMode
+	curr_re            *regexp.Regexp
+	regex_search_chan  chan string
+	string_search_chan chan string
+	search_query       string
+	write_lock         *sync.RWMutex
+}
+
+func newLogsWriter(screen tcell.Screen) logsWriter {
+	new_writer := logsWriter{
+		screen:             screen,
+		regex_search_chan:  make(chan string),
+		string_search_chan: make(chan string),
+		search_query:       "",
+		write_lock:         &sync.RWMutex{},
+	}
+	return new_writer
 }
 
 func (writer *logsWriter) Write(logs_batch []byte) (int, error) {
@@ -47,17 +70,79 @@ func (writer *logsWriter) writeSingleLog(single_log []byte) {
 	}
 	is_stdout := log_line_metadata[0] == 1
 	if !is_stdout {
-		log_line_text = elements.Colorize(log_line_text, elements.Red)
+		log_line_text = elements.Foreground(log_line_text, elements.Purple)
+	}
+	select {
+	case search := <-writer.regex_search_chan:
+		writer.search_mode = regex
+		writer.search_query = search
+		re, err := regexp.Compile(writer.search_query)
+		if err != nil {
+			fmt.Println("Failed to compile regex ", writer.search_query)
+		}
+		writer.curr_re = re
+		break
+	case search := <-writer.string_search_chan:
+		writer.search_mode = str
+		writer.search_query = search
+		break
+	default:
+		break
+	}
+	if writer.search_query != "" {
+		if writer.search_mode == regex {
+			log_line_text = writer.curr_re.ReplaceAllStringFunc(log_line_text,
+				func(s string) string { return elements.Background(s, elements.B_DarkGray) })
+		} else if writer.search_mode == str {
+			log_line_text = strings.ReplaceAll(log_line_text, writer.search_query, elements.Background(writer.search_query, elements.B_DarkGray))
+		}
+
 	}
 	fmt.Println(string(log_line_text))
 }
 
-func newLogsWriter(screen tcell.Screen) logsWriter {
-	new_writer := logsWriter{
-		draw_queue: make(chan []byte),
-		screen:     screen,
+func (writer *logsWriter) logStopper(cancel context.CancelFunc) {
+	if err := keyboard.Open(); err != nil {
+		log.Fatal("Failed to start keyboard in container log window")
 	}
-	return new_writer
+	defer keyboard.Close()
+
+	for {
+		char, key, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatalf("Got error while waiting for key '%s'", err.Error())
+		}
+		if key == keyboard.KeyEsc || key == keyboard.KeyCtrlC || char == 'q' || char == 'l' {
+			cancel()
+			if key == keyboard.KeyCtrlC {
+				os.Exit(0)
+			}
+			return
+		} else if char == '/' || char == '?' {
+			search_reader := bufio.NewReader(os.Stdin)
+			fmt.Print(string(char), " Enter search: ")
+			text, err := search_reader.ReadString('\n')
+			if err != nil {
+				log.Fatalln("Failed to read user search", err)
+			}
+			text = text[:len(text)-1] // remove newline
+			go func(query string) {
+				if query != "" {
+					if char == '?' {
+						writer.regex_search_chan <- query
+					} else {
+						writer.string_search_chan <- query
+					}
+				}
+			}(text)
+		}
+	}
+}
+
+type ContainerLogWindow struct {
+	id      string
+	context context.Context
+	cancel  context.CancelFunc
 }
 
 func NewContainerLogWindow(id string) ContainerLogWindow {
@@ -83,25 +168,6 @@ func (w *ContainerLogWindow) Close() {
 	w.cancel()
 }
 
-func logStopper(cancel context.CancelFunc) {
-
-	if err := keyboard.Open(); err != nil {
-		log.Fatal("Failed to start keyboard in container log window")
-	}
-	defer keyboard.Close()
-
-	for {
-		char, key, err := keyboard.GetSingleKey()
-		if err != nil {
-			log.Fatalf("Got error while waiting for key '%s'", err.Error())
-		}
-		if key == keyboard.KeyEsc || key == keyboard.KeyCtrlC || char == 'q' || char == 'l' {
-			cancel()
-			return
-		}
-	}
-}
-
 func (w *ContainerLogWindow) main(screen tcell.Screen) {
 	screen.Suspend()
 	_, height := screen.Size()
@@ -110,7 +176,7 @@ func (w *ContainerLogWindow) main(screen tcell.Screen) {
 	}
 	container_log_window_context, cancel := context.WithCancel(context.TODO())
 	logs_writer := newLogsWriter(screen)
-	go logStopper(cancel)
+	go logs_writer.logStopper(cancel)
 	go docker.StreamContainerLogs(w.id, &logs_writer, container_log_window_context)
 	<-container_log_window_context.Done()
 	cancel()
